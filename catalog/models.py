e15d0cf1bce7fa81b1a6dc92c198db1189f9bb80
Ksenia_Sheveleva <brantova00@gmail.com>
2025-09-14 19:41:39 +0300

    Расширена форма для продуктов

--- catalog/models.py
+++ catalog/models.py
@@ -15,13 +15,39 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Наименование товара')
-    description = models.TextField(blank=True, null=True, verbose_name='Описание товара')
-    picture = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name='Изображение')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Категория')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Наименование товара'
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Описание товара'
+    )
+    picture = models.ImageField(
+        upload_to='products/',
+        blank=True, null=True,
+        verbose_name='Изображение'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='products',
+        verbose_name='Категория'
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Цена'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата последнего изменения'
+    )
 
     class Meta:
         verbose_name = 'Товар'
