6db1d3f3232fc23eadb66dbca5a8fa6560ea2b72
Ksenia_Sheveleva <brantova00@gmail.com>
2025-10-04 23:06:03 +0300

    Добавлено кеширование и фильтрация по категориям

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig  # Импорт конфигурации приложения
 from .views import (
     HomeView,
@@ -16,7 +18,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),  # Главная страница
     path('contacts/', ContactsView.as_view(), name='contacts'),  # Страница контактов
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),  # Детальная страница продукта
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),  # Детальная страница продукта
 
     path('product/create/', ProductCreateView.as_view(), name='product_create'), # Создание
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'), # Редактирование
--- config/settings.py
+++ config/settings.py
@@ -159,3 +159,12 @@ EMAIL_HOST_USER = 'k.brantova2015@yandex.ru'
 EMAIL_HOST_PASSWORD = 'edbebkecvcfigars'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': 'redis://localhost:6379/1',
+        }
+    }
